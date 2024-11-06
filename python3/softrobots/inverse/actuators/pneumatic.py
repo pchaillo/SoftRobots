61a6346233b25447512d429f87a7c75e064788b9
pchaillo <paul.chaillou@tutanota.com>
2024-11-06 19:08:10 +0100

    need to make work the inverse exemple, then may do everything

--- python3/softrobots/inverse/actuators/pneumatic.py
+++ python3/softrobots/inverse/actuators/pneumatic.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import Sofa
 
+from softrobots.actuators import pneumatic as pb
+from softrobots.inverse.effectors import effectorGoal as eG
+
+
 def PneumaticCavity(surfaceMeshFileName=None,
                     attachedTo=None,
                     name="PneumaticCavity",
@@ -44,31 +48,14 @@ def PneumaticCavity(surfaceMeshFileName=None,
         }
 
     """
-    if attachedTo == None:
-        Sofa.msg_error("Your PneumaticCavity isn't child of any node, please set the argument attachedTo")
-        return None
-
-    if surfaceMeshFileName == None:
-        Sofa.msg_error("No surfaceMeshFileName specified, please specify one")
-        return None
-
-    # This add a component loading the topology of the cavity.
-    if surfaceMeshFileName.endswith(".stl"):
-        pneumatic.addObject('MeshSTLLoader', name='MeshLoader', filename=surfaceMeshFileName, rotation=rotation, translation=translation, scale=uniformScale)
-    elif surfaceMeshFileName.endswith(".obj"):
-        pneumatic.addObject('MeshOBJLoader', name='MeshLoader', filename=surfaceMeshFileName, rotation=rotation, translation=translation, scale=uniformScale)
-    else :
-        Sofa.msg_error("Your surfaceMeshFileName extension is not the right one, you have to give a surfacic mesh with .stl or .obj extension")
-        return None
-
-    # This add a MeshTopology, a component holding the topology of the cavity.
-    # pneumatic.addObject('MeshTopology', name="topology", filename=surfaceMeshFileName)
-    pneumatic.addObject('MeshTopology', name='topology', src='@MeshLoader')
-
-    # This add a MechanicalObject, a component holding the degree of freedom of our
-    # mechanical modelling. In the case of a cavity actuated with pneumatic, it is a set of positions specifying
-    # the points where the pressure is applied.
-    pneumatic.addObject('MechanicalObject', src="@topology")
+    pneumatic = pb.PneumaticBase(surfaceMeshFileName=surfaceMeshFileName,
+                    attachedTo=attachedTo,
+                    name=name,
+                    rotation=rotation,
+                    translation=translation,
+                    uniformScale=uniformScale,
+                    initialValue=None,
+                    valueType="pressure")
 
     # Add a SurfacePressureConstraint object with a name.
     # the indices are referring to the MechanicalObject's positions.
@@ -83,5 +70,43 @@ def PneumaticCavity(surfaceMeshFileName=None,
     # This add a BarycentricMapping. A BarycentricMapping is a key element as it will add a bi-directional link
     # between the cavity's DoFs and the parents's ones so that the pressure applied on the cavity wall will be mapped
     # to the volume structure and vice-versa;
-    pneumatic.addObject('BarycentricMapping', name="Mapping", mapForces=False, mapMasses=False)
+    # pneumatic.addObject('BarycentricMapping', name="Mapping", mapForces=False, mapMasses=False)
     return pneumatic
+
+def createScene(node):
+    from stlib3.scene import MainHeader
+
+    node.addObject('RequiredPlugin', name='Sofa.Component.IO.Mesh') # Needed to use components [MeshOBJLoader]  
+    node.addObject('RequiredPlugin', name='Sofa.Component.Mapping.Linear') # Needed to use components [BarycentricMapping]  
+    node.addObject('RequiredPlugin', name='Sofa.Component.StateContainer') # Needed to use components [MechanicalObject]  
+    node.addObject('RequiredPlugin', name='Sofa.Component.Topology.Container.Constant') # Needed to use components [MeshTopology]  
+    node.addObject('RequiredPlugin', name='Sofa.Component.Visual') # Needed to use components [VisualStyle]  
+    node.addObject('RequiredPlugin', name='Sofa.GL.Component.Rendering3D') # Needed to use components [OglSceneFrame]  
+
+    node.addObject('FreeMotionAnimationLoop')
+    node.addObject('DefaultVisualManagerLoop')    
+    node.addObject('VisualStyle', displayFlags='showVisualModels showBehaviorModels showCollisionModels hideBoundingCollisionModels showForceFields showInteractionForceFields hideWireframe')
+    
+    node.addObject('QPInverseProblemSolver', name="QP", printLog='0', saveMatrices = True ,epsilon = 0.01) # initialement epsilon = 0.001
+
+    MainHeader(node, plugins=["SoftRobots", 'SofaPython3'])
+    node.addObject('MechanicalObject')
+
+    pneumatic = PneumaticCavity(surfaceMeshFileName="mesh/cube.obj", attachedTo=node)
+    pneumatic.addObject('TriangleCollisionModel', moving='0', simulated='1') # For visualisation
+    pneumatic.addObject('TriangleFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.49,  youngModulus=10000, thickness = 5) # stable youngModulus = 500 / réel ? = 103
+    pneumatic.addObject('SparseLDLSolver', name='ldlsolveur',template="CompressedRowSparseMatrixMat3x3d")
+    pneumatic.addObject('GenericConstraintCorrection')
+    pneumatic.addObject('EulerImplicitSolver', firstOrder='1', vdamping=0)
+    pneumatic.addObject('RestShapeSpringsForceField', points=[0,1,2,3], angularStiffness=1e5, stiffness=1e5) # pour accrocher la base du robot dans l'espace
+    pneumatic.addObject('UniformMass', totalMass=1000, rayleighMass = 0)
+
+    goal = eG.EffectorGoal(attachedTo=node, position = [0,0,0],name = 'goal')
+    goal.addObject('SphereCollisionModel', radius=2) # for visualisation
+    controlledPoints = pneumatic.addChild('controlledPoints')
+    controlledPoints.addObject('MechanicalObject', name="actuatedPoints", template="Vec3",position=[0, 0, 0])#,rotation=[0, 90 ,0]) # classic
+    # controlledPoints.addObject('MechanicalObject', name="actuatedPoints", template="Rigid3",position=[0, 0, h_effector,0., 0., 0., 1.])#,rotation=[0, 90 ,0]) # rigid pour l'orientation
+    controlledPoints.addObject('PositionEffector', template="Vec3d", indices='0', effectorGoal="@../../../goal/goalM0.position") # classic
+    controlledPoints.addObject('BarycentricMapping', mapForces=False, mapMasses=False)
+
+    # node.addObject(PressureController(pas=10,parent = pneumatic))
